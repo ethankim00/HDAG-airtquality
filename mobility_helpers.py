3d8cc9fe3a6dce8d27a50fd901659a6b9b3ea7b3
Ethan Kim <ethan_kim@college.harvard.edu>
2020-07-07 22:31:24 -0400

    Added full data sources

--- mobility_helpers.py
+++ mobility_helpers.py
@@ -7,7 +7,7 @@ import seaborn as sns
 def plot_metric(mobility_data,state, county, metric):
     """ Generate Plot of a mobility metric over time """
     # TODO - plot actual date instead of number of days
-    data = mobility_data[(mobility_data['sub_region_1'] == state) &(mobility_data['sub_region_2'] == county)]
+    data = mobility_data[(mobility_data['state'] == state) & (mobility_data['county'] == county)]
     fig, ax = plt.subplots(1,1, figsize = (15, 10))
     ax.set_title('{} county, {}: {}'.format(county, state, metric), size = 35)
     ax.plot(data[metric], color = 'purple', alpha = 0.5)
